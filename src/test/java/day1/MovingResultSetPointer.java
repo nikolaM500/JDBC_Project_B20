4eb60c833eabf25a20c3fad3eea1045e1c48072e
Akbar <Akbar@cybertekschool.com>
2020-11-22 16:06:50 -0500

    ResultSet that can go forward and backward

---
+++ src/test/java/day1/MovingResultSetPointer.java
@@ -0,0 +1,22 @@
+package day1;
+
+import java.sql.*;
+
+public class MovingResultSetPointer {
+
+    public static void main(String[] args) throws SQLException {
+
+        // we want to create a statement object that generate
+        // ResultSet that can move forward and backward anytime
+        String connectionStr = "jdbc:oracle:thin:@52.201.187.226:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+        Statement stmnt = conn.createStatement();
+        ResultSet rs   =   stmnt.executeQuery("SELECT * FROM REGIONS") ;
+
+
+    }
+
+}
