6f9cf08d5b8273077baced18c746b5c070a13a64
Akbar <Akbar@cybertekschool.com>
2020-11-24 20:42:16 -0500

    JDBC Day2 - Displaying All Data from All Column : Starter

---
+++ src/test/java/day2/DisplayingAllDataFromAllColumns.java
@@ -0,0 +1,21 @@
+package day2;
+
+import java.sql.*;
+
+public class DisplayingAllDataFromAllColumns {
+
+    public static void main(String[] args) throws SQLException {
+
+        String connectionStr = "jdbc:oracle:thin:@52.201.187.226:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+        Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet rs   =   stmnt.executeQuery("SELECT * FROM EMPLOYEES") ;
+
+
+
+    }
+
+}
