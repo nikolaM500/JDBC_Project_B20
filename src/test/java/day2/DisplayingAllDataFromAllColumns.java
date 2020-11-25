21cf1a9758da5fc6d4e4f4d7f543ab90eb3f893d
Akbar <Akbar@cybertekschool.com>
2020-11-24 20:54:40 -0500

    JDBC Day2 - Displaying All Data from All Column : Finished

--- src/test/java/day2/DisplayingAllDataFromAllColumns.java
+++ src/test/java/day2/DisplayingAllDataFromAllColumns.java
@@ -14,6 +14,34 @@ public class DisplayingAllDataFromAllColumns {
         Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet rs   =   stmnt.executeQuery("SELECT * FROM EMPLOYEES") ;
 
+        // print out entire first row of Employee table from above query
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount() ;
+
+        // print out column name in the beginning row , then print first row
+
+        for (int colNum = 1; colNum <= columnCount; colNum++) {
+            System.out.print( rsmd.getColumnLabel(colNum) + "\t"  );
+        }
+        System.out.println("----------------------------");
+        rs.next() ;
+
+        // this whole loop is getting one row of data
+        for (int colNum = 1; colNum <= columnCount; colNum++) {
+            System.out.print(rs.getString( colNum ) + "\t" );
+        }
+
+        // now how do you get all the row if you know how to get one row ???
+        // I want to go from the first row till the last row and print all columns
+        rs.beforeFirst();
+
+        while (rs.next() ){
+
+            for (int colNum = 1; colNum <= columnCount; colNum++) {
+                System.out.print(rs.getString( colNum ) + "\t" );
+            }
+            System.out.println();
+        }
 
 
     }
