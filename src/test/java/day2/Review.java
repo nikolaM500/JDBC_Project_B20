43fbb40c00097512402f0a1d2640c6f353daaf32
Akbar <Akbar@cybertekschool.com>
2020-11-24 20:11:41 -0500

    JDBC Day2 - Closing resources after usage rs.close(); stmnt.close(); conn.close();

--- src/test/java/day2/Review.java
+++ src/test/java/day2/Review.java
@@ -49,6 +49,11 @@ public class Review {
             System.out.println("MIN SALARY COLUMN AS NUMBER  $" + rs.getDouble("MIN_SALARY")    );
         }
 
+        // clean up the connection , statement and resultset object after usage
+        rs.close();
+        stmnt.close();
+        conn.close();
+
 
 
 
